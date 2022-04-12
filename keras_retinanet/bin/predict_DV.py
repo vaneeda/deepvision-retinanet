8e2bf6a431bbfb688d5b568c78001d79e6bd05df
Allken Vaneeda <vaneeda.allken@hi.no>
2022-04-12 19:00:40 +0200

    Corrected name of config file

--- keras_retinanet/bin/predict_DV.py
+++ keras_retinanet/bin/predict_DV.py
@@ -97,7 +97,7 @@ def DV_predict(model, path_to_data, xml_file):
 
 
 if __name__ == '__main__':
-    PARAMS = load_config(config_path=os.path.join(os.path.dirname(__file__), 'detect_config_local.yaml'))
+    PARAMS = load_config(config_path=os.path.join(os.path.dirname(__file__), 'detect_config.yaml'))
     labels_to_names = {}
     with open("fish_4classes.csv", mode='r') as inp:
         reader = csv.reader(inp)
