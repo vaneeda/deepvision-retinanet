41c8b9274b7631d270f3e392e7c398b3f252dac3
atempelaar <alex.tempelaar@gmail.com>
2022-05-23 12:38:29 +0200

    Remove tensorflow INFO printing and keep only >=ERROR level
    Instead of printing all the INFO messages generated by Tf, only print the messages from ERROR level and greater. Added also a message showing what GPUs are available, indicating that Tf will se one when processing.

--- keras_retinanet/bin/predict_DV.py
+++ keras_retinanet/bin/predict_DV.py
@@ -1,4 +1,13 @@
 import os
+# Change log level for tensorflow
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+"""
+Levels: 
+    0 = all messages are logged (default behavior)
+    1 = INFO messages are not printed
+    2 = INFO and WARNING messages are not printed
+    3 = INFO, WARNING, and ERROR messages are not printed
+"""
 import sys
 if __name__ == "__main__" and __package__ is None:
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
@@ -25,6 +34,18 @@ def get_session():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     return tf.Session(config=config)
+
+
+from tensorflow.python.client import device_lib
+def print_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    devs = [x.physical_device_desc for x in local_device_protos if x.device_type in ['GPU']]
+    if devs:
+        print(f"Available GPUs:")
+        for d in devs:
+            print(f" - {d}")
+    else:
+        print("NO available GPU.")
 
 
 def dv_xml_to_csv(xml_file):
@@ -104,6 +125,7 @@ def DV_predict(model, path_to_data, score_threshold, orientation, xml_file):
 
 
 if __name__ == '__main__':
+    print_available_gpus()
     PARAMS = load_config(config_path=os.path.join(os.path.dirname(__file__), 'detect_config.yaml'))
     labels_to_names = PARAMS["classes"]
     model = models.load_model(PARAMS["snapshot_path"], backbone_name='resnet50', compile=False)
