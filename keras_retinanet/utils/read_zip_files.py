122bc51e295568777ed2a2caf3b4b58cc1321d81
a5515 <a5515@hi.no>
2022-05-24 19:25:27 +0200

    Read zip files

---
+++ keras_retinanet/utils/read_zip_files.py
@@ -0,0 +1,13 @@
+import os
+import zipfile
+
+
+def read_zip_files(path_to_data):
+    list_of_files = []
+    zip_dict = {}
+    zip_files = [x for x in os.listdir(path_to_data) if x.endswith("active.zip")]
+    for z in zip_files:
+        archive = zipfile.ZipFile(os.path.join(path_to_data, z), "r")
+        zip_dict[z] = archive.namelist()
+        list_of_files += archive.namelist()
+    return zip_dict, list_of_files
